65396d7d15aba47d683e8c1480818b5197fbad85
hime <dasha.motry2002@mail.ru>
2024-06-23 10:54:21 +0300

    done

--- src/TenHomeTaskk.java
+++ src/TenHomeTaskk.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class TenHomeTaskk {
+public class TenHomeTaskk {
+    public static void main(String[] args) {
+    //task1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " +  firstName + " " + middleName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+     //task2
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета - " + fullName.toUpperCase());
+    //task3
+        fullName = "Иванов Семён Семёнович";
+        String name = fullName.replace('ё', 'е');
+        System.out.println("Данные Ф.И.О. сотрудника - " + name);
+        //System.out.println("Данные Ф.И.О. сотрудника - " + fullName.replace('ё', 'е'));
+    }
 }
